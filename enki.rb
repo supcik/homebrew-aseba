e3878fdad0b59d476e161e61981e2996df0c0514
Stéphane Magnenat <stephane@magnenat.net>
2014-08-25 11:07:36 +0200

    Reverting change that does not work.

--- enki.rb
+++ enki.rb
@@ -9,7 +9,7 @@ class Enki < Formula
   depends_on "qt"
   depends_on "sdl"
   depends_on :python => :optional
-  depends_on :boost => "with-python"
+  depends_on "boost" => "with-python"
 
   def install
     system "cmake", ".", *std_cmake_args, "-DPYTHON_CUSTOM_TARGET=#{prefix}/lib/python2.7/site-packages"
