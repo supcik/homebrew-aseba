1fc027e0a56e6c18b567cc07cb83d795df3fab40
Stéphane Magnenat <stephane@magnenat.net>
2014-08-25 10:41:43 +0200

    Added formulas to build Aseba.

---
+++ aseba.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class Aseba < Formula
+  homepage "http://aseba.wikidot.com"
+  url "https://github.com/aseba-community/aseba/archive/1.3.3.tar.gz"
+  sha1 "023ade01c6d859f4e97a90546bc6f5de3adf6ebb"
+  head "https://github.com/aseba-community/aseba.git"
+
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "qwt"
+  depends_on "dashel"
+  depends_on "enki"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/asebacmd"
+  end
+end
