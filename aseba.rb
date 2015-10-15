83e2f13ca0af20fbf0f16bfb0639523ea21d9458
Jacques Supcik <jacques.supcik@hefr.ch>
2015-10-15 22:49:30 +0200

    changes for 1.4.00.bmc

--- aseba.rb
+++ aseba.rb
@@ -2,14 +2,9 @@ require "formula"
 
 class Aseba < Formula
   homepage "http://aseba.wikidot.com"
-  url "https://github.com/aseba-community/aseba/archive/1.3.3.tar.gz"
-  sha1 "023ade01c6d859f4e97a90546bc6f5de3adf6ebb"
-  head "https://github.com/aseba-community/aseba.git"
+  url "https://github.com/supcik/aseba.git", :branch => "dev"
 
-  head do
-    depends_on "protobuf"
-  end
-  
+  depends_on "protobuf"
   depends_on "cmake" => :build
   depends_on "qt"
   depends_on "qwt"
